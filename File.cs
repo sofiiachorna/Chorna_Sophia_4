e830c94f468faa5885691a88f5cd534182d57df8
sofiiachorna <sonya.chorna@gmail.com>
2022-06-02 10:00:54 +0300

    first commit

---
+++ File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4_oop
+{
+    public class File : Disk
+    {
+        public File (int size, string name) : base(size) 
+        { 
+            this.size = size;
+            this.name = name;
+        }
+        public override void Launch()
+        {
+            Console.WriteLine($"File {name} successfully launched");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab_4_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            directory dir = new directory(800, "files");
+            dir.add(new File(101, "file1.txt"));
+            dir.add(new archive(150, "Photos.rar"));
+            dir.add(new File(5000, "FlappyBird.exe"));
+            dir.add(new mp3(3000, "Music.mp3"));
+            dir.add(new mp3(10000, "song.mp3"));
+
+
+            Console.WriteLine(dir.countmp3());
+
+            dir.GetAllTypes();
+            Console.WriteLine("Input the name of file you want to launch:");
+            string a = Console.ReadLine();
+            dir.LaunchFile(a);
+
+            Console.WriteLine($"Directoty {dir.name} has size {dir.getsize()} and length {dir.getLength()}");
+        }
+    }
+}
